0a8b2fb6e467fd2b3a9ed0ce6e75f3c8e05681eb
Gregory Caloin <gregchapauli@gmail.com>
2020-07-28 18:34:17 +0200

    Activation des boutons et finalisation

--- script.js
+++ script.js
@@ -14,24 +14,23 @@ $(function(){
 
         $mainMenuItems.children(".images").click(function(){
 
-            var newIndex = $(this).parent().index(),
+            var newIndex = $(this).parent().index();
             $item = $mainMenuItems.eq(newIndex);
+            checkAndAnimateItem(newIndex);
+        });
 
-            if(openedIndex === newIndex)
-            {
-                animateItem($item, false, 250);
-                openedIndex = -1;
-            }
-            else
-            {
-                if(validIndex(newIndex))
-                {
-                    animateItem($mainMenuItems.eq(openedIndex), false, 250);
-                    openedIndex = newIndex;
-                    animateItem($item, true, 250);
+        $(".button").hover(
+        function(){
+            $(this).addClass("hovered");
+        },
+        function(){
+            $(this).removeClass("hovered");
+        }
+        );
 
-                }
-            }
+        $(".button").click(function(){
+            var newIndex = $(this).index();
+            checkAndAnimateItem(newIndex);
         });
     },
 
@@ -45,6 +44,25 @@ $(function(){
         colorImageParam = toOpen ? {left: "0px"}:{left: "140px"};
         $colorImage.animate(colorImageParam,speed);
         $item.animate(itemParam,speed);
+    },
+    
+    checkAndAnimateItem = function(indexToCheckAndAnimate){
+        if(openedIndex === indexToCheckAndAnimate)
+        {
+            animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+            openedIndex = -1;
+        }
+        else
+        {
+            if(validIndex(indexToCheckAndAnimate))
+            {
+                animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                openedIndex = indexToCheckAndAnimate;
+                animateItem($mainMenuItems.eq(openedIndex), true, 250);
+
+            }
+        }
+
     };
 
     init();
